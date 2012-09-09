04a83e65005e208d4df24b8a325d5011bbd3c735
NatashaMurashev <nmurashev@gmail.com>
2012-09-09 11:52:01 -0700

    updated database host

--- icandothis/settings.py
+++ icandothis/settings.py
@@ -16,8 +16,8 @@ DATABASES = {
         'NAME': 'icandjango',                      # Or path to database file if using sqlite3.
         'USER': 'nataliamurashev',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5433',                      # Set to empty string for default. Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
